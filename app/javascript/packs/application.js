4ae477e30fcea9f312cce3c99fa15225ca8c8189
James Devereux <devereuxjj@gmail.com>
2019-12-02 14:47:29 +0900

    bootstrap is loading but learn-plus theme styles are not

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,78 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+
+import 'assets/javascripts/app-icons.js'
+import 'assets/javascripts/app-settings.js'
+import 'assets/javascripts/app.js'
+import 'assets/javascripts/chartjs-rounded-bar.js'
+import 'assets/javascripts/chartjs.js'
+import 'assets/javascripts/check-selected-row.js'
+import 'assets/javascripts/countdown.js'
+import 'assets/javascripts/dragula.js'
+import 'assets/javascripts/dropdown-dismissable.js'
+import 'assets/javascripts/fancytree.js'
+import 'assets/javascripts/flatpickr.js'
+import 'assets/javascripts/fullcalendar.js'
+import 'assets/javascripts/hljs.js'
+import 'assets/javascripts/learnplus-app-settings-mixin.js'
+import 'assets/javascripts/list.js'
+import 'assets/javascripts/map-single-marker.js'
+import 'assets/javascripts/map.js'
+import 'assets/javascripts/messages.js'
+import 'assets/javascripts/nestable.js'
+import 'assets/javascripts/quill.js'
+import 'assets/javascripts/settings.js'
+import 'assets/javascripts/sweetalert.js'
+import 'assets/javascripts/toggle-check-all.js'
+import 'assets/javascripts/touchspin.js'
+import 'assets/javascripts/vector-maps.js'
+
+import 'assets/stylesheets/app.css'
+import 'assets/stylesheets/app.rtl.css'
+import 'assets/stylesheets/bootstrap-touchspin.css'
+import 'assets/stylesheets/bootstrap-touchspin.rtl.css'
+import 'assets/stylesheets/dropzone.css'
+import 'assets/stylesheets/dropzone.rtl.css'
+import 'assets/stylesheets/fancytree.css'
+import 'assets/stylesheets/fancytree.rtl.css'
+import 'assets/stylesheets/flatpickr-airbnb.css'
+import 'assets/stylesheets/flatpickr-airbnb.rtl.css'
+import 'assets/stylesheets/flatpickr.css'
+import 'assets/stylesheets/flatpickr.rtl.css'
+import 'assets/stylesheets/fontawesome.css'
+import 'assets/stylesheets/fontawesome.rtl.css'
+import 'assets/stylesheets/fullcalendar.css'
+import 'assets/stylesheets/fullcalendar.rtl.css'
+import 'assets/stylesheets/jvectormap.css'
+import 'assets/stylesheets/jvectormap.rtl.css'
+import 'assets/stylesheets/material-icons.css'
+import 'assets/stylesheets/material-icons.rtl.css'
+import 'assets/stylesheets/nestable.css'
+import 'assets/stylesheets/nestable.rtl.css'
+import 'assets/stylesheets/quill.css'
+import 'assets/stylesheets/quill.rtl.css'
+import 'assets/stylesheets/sweetalert.css'
+import 'assets/stylesheets/sweetalert.rtl.css'
+
+import 'assets/vendor/perfect-scrollbar.min.js'
+import 'assets/vendor/dom-factory.js'
+import 'assets/vendor/material-design-kit.js'
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
